026e0c099dfa65d4da6ebb9935cd40b2825011d8
DeepAnchor <xdeepanchor@gmail.com>
2016-12-30 16:06:47 -0800

    make scheme shared for Carthage compatibility

---
+++ Example/build/Pods.build/Debug-iphonesimulator/Pods-MYTableViewIndex_Example.build/DerivedSources/Pods_MYTableViewIndex_Example_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_MYTableViewIndex_ExampleVersionString[];
+ extern const double Pods_MYTableViewIndex_ExampleVersionNumber;
+
+ const unsigned char Pods_MYTableViewIndex_ExampleVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_MYTableViewIndex_Example  PROJECT:Pods-1" "\n";
+ const double Pods_MYTableViewIndex_ExampleVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ Example/build/Pods.build/Debug-iphonesimulator/Pods-MYTableViewIndex_Tests.build/DerivedSources/Pods_MYTableViewIndex_Tests_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_MYTableViewIndex_TestsVersionString[];
+ extern const double Pods_MYTableViewIndex_TestsVersionNumber;
+
+ const unsigned char Pods_MYTableViewIndex_TestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_MYTableViewIndex_Tests  PROJECT:Pods-1" "\n";
+ const double Pods_MYTableViewIndex_TestsVersionNumber __attribute__ ((used)) = (double)1.;
